37961480d16b412268cfc6dc92fd83b227cd0b89
Paulo Lisboa <paulolisboa38@gmail.com>
2023-01-09 08:12:22 -0300

    new class PosicaoXadrez

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -6,20 +6,25 @@ namespace XadrezConsole
     {
         static void Main (string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8,8);
+            PosicaoXadrez posicaoXadrez = new PosicaoXadrez('c',7);
+            Console.WriteLine(posicaoXadrez);
 
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro),new Posicao(0,0));
-                tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro),new Posicao(1,3));
-                tabuleiro.ColocarPeca(new Rei(Cor.Preta,tabuleiro),new Posicao(0,2));
+            Console.WriteLine(posicaoXadrez.ToPosicao());
 
-                Tela.ImprimirTabuleiro(tabuleiro);
-            }
-            catch (TabuleiroException t)
-            {
-                Console.WriteLine(t.Message);
-            }
+            //try
+            //{
+            //    Tabuleiro tabuleiro = new Tabuleiro(8,8);
+
+            //    tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro),new Posicao(0,0));
+            //    tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro),new Posicao(1,3));
+            //    tabuleiro.ColocarPeca(new Rei(Cor.Preta,tabuleiro),new Posicao(0,2));
+
+            //    Tela.ImprimirTabuleiro(tabuleiro);
+            //}
+            //catch (TabuleiroException t)
+            //{
+            //    Console.WriteLine(t.Message);
+            //}
         }
     }
 }
---
+++ XadrezConsole/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,23 @@
+﻿using XadrezConsole.tabuleiro;
+
+namespace XadrezConsole.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+        public PosicaoXadrez (char coluna,int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        public Posicao ToPosicao ()
+        {
+            return new Posicao(8 - Linha,Coluna - 'a');
+        }
+        public override string? ToString ()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
